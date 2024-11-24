d29434345e161606ff7efc2c590816131a0d89c4
thivankas@outlook.com <tsomacha@c1r6p18.hive.fi>
2024-11-24 17:00:58 +0200

    Refactorig and fixing return values for error

---
+++ srcs/ft_pointer.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pointer.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 13:14:14 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/11/24 16:49:00 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_pointer(uintptr_t nbr)
+{
+	int	length;
+
+	length = 0;
+	if (nbr < 16)
+		length += ft_printdigit(nbr, 0);
+	else
+	{
+		length += ft_pointer((nbr / 16));
+		length += ft_printdigit((nbr % 16), 0);
+	}
+	return (length);
+}
+
+int	ft_printpointer(uintptr_t nbr)
+{
+	int	length;
+
+	length = 0;
+	if (nbr == 0)
+	{
+		length += ft_printstr("(nil)");
+	}
+	else
+	{
+		length += ft_printstr("0x");
+		length += ft_pointer(nbr);
+	}
+	return (length);
+}
