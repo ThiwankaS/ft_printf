b806c02951bd4949303f2188396486443fbedead
thivankas@outlook.com <tsomacha@c2r6p1.hive.fi>
2024-11-28 11:46:29 +0200

    Fix normintte errors

--- srcs/ft_pointer.c
+++ srcs/ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 13:14:14 by tsomacha          #+#    #+#             */
-/*   Updated: 2024/11/24 16:49:00 by tsomacha         ###   ########.fr       */
+/*   Updated: 2024/11/28 11:41:51 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_pointer(uintptr_t nbr)
 	if (nbr < 16)
 	{
 		res = ft_printdigit(nbr, 0);
-		if(res < 0)
+		if (res < 0)
 			return (-1);
 		len += res;
 	}
@@ -30,7 +30,7 @@ int	ft_pointer(uintptr_t nbr)
 	{
 		res = ft_pointer((nbr / 16));
 		len = ft_printdigit((nbr % 16), 0);
-		if(res < 0 || len < 0)
+		if (res < 0 || len < 0)
 			return (-1);
 		len += res;
 	}
@@ -47,7 +47,7 @@ int	ft_printpointer(uintptr_t nbr)
 	if (nbr == 0)
 	{
 		res = ft_printstr("(nil)");
-		if(res < 0)
+		if (res < 0)
 			return (-1);
 		len += res;
 	}
@@ -55,7 +55,7 @@ int	ft_printpointer(uintptr_t nbr)
 	{
 		res = ft_printstr("0x");
 		len = ft_pointer(nbr);
-		if(res < 0 || len < 0)
+		if (res < 0 || len < 0)
 			return (-1);
 		len += res;
 	}
